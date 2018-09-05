94812de44865a0805a667f38f9a9b89225a73aaf
Giannoulo <giannoulopoulosg@gmail.com>
2018-09-05 16:54:39 +0300

    1111111

---
+++ MainCluster.py
@@ -0,0 +1,48 @@
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+import haversine
+
+def port_visit(row):
+
+
+    lat = row['lat']
+    lon = row['lon']
+
+    port_list = br_ports.value
+    visitors_list = []
+    for i in port_list:
+        distance = haversine((lat,lon), (i[1], i[0]), miles=False)
+        if (distance < 0.5):
+             visitors_list.append(row['sourcemmsi'])
+             break
+
+    return visitors_list
+
+
+
+
+# Create the spark session object
+conf = SparkConf().setAppName('Shipyard')
+# sc = SparkContext(conf=conf)
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
+#Read the input data to an RDD
+# ais_data = sc.textFile('file:///C:\\Users\\SinQ\\PycharmProjects\\shipyard\\nari_dynamic_1million.csv')
+ais_data = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load('hdfs://Master1/user/nari_dynamic_1million.csv')
+
+#Read the ports csv, turn it to a list of tuples and broadcast it to the workers
+ports_df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load('hdfs://Master1/user/ports.csv')
+port_tuples = [tuple(x) for x in ports_df.collect()]
+br_ports = spark.sparkContext.broadcast(port_tuples)
+
+#Check if the point is close to a port and save the mmsi
+port_visitors = ais_data.rdd.flatMap(port_visit)
+
+
+unique_visitors = port_visitors.distinct()
+unique_list = unique_visitors.collect()
+
+for x in unique_list:
+    print x
+print len(unique_list)
